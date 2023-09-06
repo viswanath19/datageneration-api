92c26be31ca3c50dd30a904825831bd0e4a0b7f6
Viswanath <allamrajuviswanath@gmail.com>
2023-09-06 20:27:56 +0530

    datasets and initial endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const nameDataset = require('../datasets/name_dataset.json');
 const mainRouter = express.Router();
 
 mainRouter.get("/",(req,res) => {
@@ -7,7 +7,14 @@ mainRouter.get("/",(req,res) => {
 })
 
 mainRouter.get("/generateNames",(req,res)=>{
-    res.send("Name Generation Started");
+    let names = "";
+    const processStart = Math.floor(Math.random()*(35000-1)+1);
+    const processEnd = processStart + 50;
+    let nameLimiter = nameDataset.slice(processStart, processEnd);
+    nameLimiter.forEach((item)=>{
+        names = names + item.people_name + "<br/>";
+    })
+    res.status(200).send(names);
 })
 
 module.exports = mainRouter;
