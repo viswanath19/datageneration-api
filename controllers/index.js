90e619c52b27454927dbed676c04c28280a90cec
Viswanath Sarma Allamraju <allamrajuviswanath@gmail.com>
2023-09-07 16:06:42 +0530

    Update index.js async await changes

--- controllers/index.js
+++ controllers/index.js
@@ -6,10 +6,10 @@ mainRouter.get("/",(req,res) => {
     res.send("Hello From Ankit Main Router");
 })
 
-mainRouter.get("/generateNames",(req,res)=>{
+mainRouter.get("/generateNames",async (req,res)=>{
     let names = "";
-    //const mobileNumberFilters = nameDataset.filter((item)=>{return Object.keys(item).indexOf("mobile_number") > -1});
-    const processStart = Math.floor(Math.random()*(35000-1)+1);
+    const mobileNumberFilters = await nameDataset.filter((item)=>{return Object.keys(item).indexOf("mobile_number") > -1});
+    /*const processStart = Math.floor(Math.random()*(35000-1)+1);
     const processEnd = processStart + 50;
     let nameLimiter = nameDataset.slice(processStart, processEnd);
     nameLimiter.forEach((item)=>{
@@ -24,8 +24,8 @@ mainRouter.get("/generateNames",(req,res)=>{
             rowData = rowData + item["mobile_number"] + ","
         }
         names = names + rowData + "<br/>";
-    })
-    res.status(200).send(names);
+    })*/
+    res.status(200).send(mobileNumberFilters);
 })
 
 module.exports = mainRouter;
