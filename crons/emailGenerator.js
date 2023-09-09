7e5712ff5db2497fe70830d37db708dfee66844c
Viswanath <allamrajuviswanath@gmail.com>
2023-09-09 15:24:09 +0530

    email generation logic updated

--- crons/emailGenerator.js
+++ crons/emailGenerator.js
@@ -5,13 +5,17 @@ const generateEmailAddresses = () => {
     console.log("path as>>>",__dirname);
     dbClient.query("SELECT count(email_addresses) FROM dataccrue_dataset.email_dataset").then(results => {
         dbClient.query("SELECT people_name FROM dataccrue_dataset.name_dataset limit 1 OFFSET "+results.rows[0]['count']).then(results => {
-            dbClient.query("INSERT INTO dataccrue_dataset.email_dataset (email_addresses) VALUES ($1) RETURNING *",[results.rows[0]["people_name"].toLowerCase().replaceAll(" ",".") + "@gmail.com"],(err,results) => {
-                if (err) {
-                    console.log("Error Occured while Inserting email address",err);
-                } else {
-                    console.log("Email Address Inserted",results.rows[0]);
-                }
-            });
+            if (Array.isArray(results.rows) && results.rows.length > 0 && typeof results.rows[0] === 'string') {
+                dbClient.query("INSERT INTO dataccrue_dataset.email_dataset (email_addresses) VALUES ($1) RETURNING *",[results.rows[0]["people_name"] + "@gmail.com".toLowerCase().replaceAll(" ",".")],(err,results) => {
+                    if (err) {
+                        console.log("Error Occured while Inserting email address",err);
+                    } else {
+                        console.log("Email Address Inserted",results.rows[0]);
+                    }
+                });
+            } else {
+                console.log("Something went wrong");
+            }
         }).catch(err => {
             console.log("Error occured while fetch the name",err);
         });
