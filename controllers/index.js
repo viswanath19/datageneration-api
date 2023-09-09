d1fe5130fb698ca7213f4d51eeb23c90d41b4d25
Viswanath <allamrajuviswanath@gmail.com>
2023-09-09 19:43:00 +0530

    fetch controller updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const nameDataset = require('../datasets/name_dataset.json');
 const dbClient = require('../database/index');
 const mainRouter = express.Router();
 
@@ -8,20 +7,36 @@ mainRouter.get("/",(req,res) => {
 })
 
 mainRouter.get("/generateNames",async (req,res)=>{
-    let names = "";
+    let names = "<table><tr><th>Name</th><th>Email Address</th><th>Phone Number</th></tr>";
     const offset = Math.floor(Math.random()*(35000-1)+1);
     const fetchNames = await dbClient.query("SELECT people_name FROM dataccrue_dataset.name_dataset limit 50 OFFSET "+offset).then(results => {
-        console.log("results as follows",results.rowCount);
+        console.log("results as follows from names db",results.rowCount);
         return results.rows;
     }).catch(err => {
-        console.log("Error Occured",err);
+        console.log("Error Occured While Fetching in Names db",err);
     });
-    console.log("fetchNames",fetchNames);
+    const fetchEmails = await dbClient.query("SELECT email_addresses FROM dataccrue_dataset.email_dataset limit 50").then(results => {
+        console.log("results as follows from emails db",results.rowCount);
+        return results.rows;
+    }).catch(err => {
+        console.log("Error Occured While Fetching in emails db",err);
+    });
+    const fetchPhoneNumbers = await dbClient.query("SELECT mobile_numbers FROM dataccrue_dataset.phone_dataset limit 50").then(results => {
+        console.log("results as follows from phones db",results.rowCount);
+        return results.rows;
+    }).catch(err => {
+        console.log("Error Occured While Fetching in Phones db",err);
+    });
     if (fetchNames !== null && fetchNames.length > 0) {
-        fetchNames.forEach((rowItem) => {
-            names = names + rowItem['people_name'] + "<br/>";
+        fetchNames.forEach((rowItem,index) => {
+            names = names + `<tr>
+                    <td>${rowItem['people_name']}</td>
+                    <td>${fetchEmails[index]["email_addresses"]}</td>
+                    <td>${fetchPhoneNumbers[index]["mobile_numbers"]}</td>
+                    </tr>`;
         })
     }
+    names = names + "</table>"
     res.status(200).send(names);
 })
 
