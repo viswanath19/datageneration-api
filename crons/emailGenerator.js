de085e255b7bb0920b2b5cf6f2f7ac16b74febab
Viswanath <allamrajuviswanath@gmail.com>
2023-09-09 15:49:41 +0530

    email generation logic updates

--- crons/emailGenerator.js
+++ crons/emailGenerator.js
@@ -7,7 +7,7 @@ const generateEmailAddresses = () => {
         dbClient.query("SELECT people_name FROM dataccrue_dataset.name_dataset limit 1 OFFSET "+results.rows[0]['count']).then(results => {
             console.log("Is array ",Array.isArray(results.rows),"Array length",results.rows.length,"typeof string",typeof results.rows[0]);
             if (Array.isArray(results.rows) && results.rows.length > 0 && typeof results.rows[0] === 'object' && typeof results.rows[0]["people_name"] === 'string') {
-                dbClient.query("INSERT INTO dataccrue_dataset.email_dataset (email_addresses) VALUES ($1) RETURNING *",[results.rows[0]["people_name"] + "@gmail.com".toLowerCase().replace(/ /g,".")],(err,results) => {
+                dbClient.query("INSERT INTO dataccrue_dataset.email_dataset (email_addresses) VALUES ($1) RETURNING *",[results.rows[0]["people_name"].concat("@gmail.com").toLowerCase().replace(/ /g,".")],(err,results) => {
                     if (err) {
                         console.log("Error Occured while Inserting email address",err);
                     } else {
