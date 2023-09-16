ebd2d581ce9fb0d0852a7b0b9af2ad550c81ae5d
Viswanath <allamrajuviswanath@gmail.com>
2023-09-16 21:47:12 +0530

    fetch controller logic updated

--- controllers/index.js
+++ controllers/index.js
@@ -16,34 +16,33 @@ mainRouter.get("/generateNames",async (req,res)=>{
     }).catch(err => {
         console.log("Error Occured While Fetching in Names db",err);
     });
-    const fetchEmailsCounts = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.email_dataset").then(results => {
-        console.log("no of email rows",results.rows);
-        const offset = Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
-        dbClient.query("SELECT email_addresses FROM dataccrue_dataset.email_dataset limit 50 OFFSET "+offset-50).then(results => {
-            console.log("results as follows from emails db",results.rowCount);
-            return results.rows;
-        }).catch(err => {
-            console.log("Error Occured While Fetching in emails db",err);
-        });
+    const fetchEmailCounts = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.email_dataset").then(results => {
+        return Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
     }).catch(err => {
         console.log("Error Occured While Fetching in emails db counts",err);
     })
+
+    const fetchEmails = await dbClient.query("SELECT email_addresses FROM dataccrue_dataset.email_dataset limit 50 OFFSET "+fetchEmailCounts).then(results => {
+        console.log("results as follows from emails db",results.rowCount);
+        return results.rows;
+    }).catch(err => {
+        console.log("Error Occured While Fetching in emails db",err);
+    });
     
-    const fetchPhoneNumbers = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.phone_dataset").then(results => {
-        const offset = Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
-        dbClient.query("SELECT mobile_numbers FROM dataccrue_dataset.phone_dataset limit 50 OFFSET "+offset-50).then(results => {
-            console.log("results as follows from phones db",results.rowCount);
-            return results.rows;
-        }).catch(err => {
-            console.log("Error Occured While Fetching in Phones db",err);
-        });
+    const fetchPhoneNumberCounts = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.phone_dataset").then(results => {
+        return Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
     }).catch(err => {
         console.log("Error Occured While Fetching in phone db counts",err);
     });
-    console.log("fetch emails",fetchEmails);
+    const fetchPhoneNumbers = await dbClient.query("SELECT mobile_numbers FROM dataccrue_dataset.phone_dataset limit 50 OFFSET "+fetchPhoneNumberCounts).then(results => {
+        console.log("results as follows from phones db",results.rowCount);
+        return results.rows;
+    }).catch(err => {
+        console.log("Error Occured While Fetching in Phones db",err);
+    });
+    
     if (fetchNames !== null && fetchNames.length > 0) {
         fetchNames.forEach((rowItem,index) => {
-            console.log(fetchEmails[index])
             names = names + `<tr>
                     <td>${rowItem['people_name']}</td>
                     <td>${fetchEmails[index]["email_addresses"]}</td>
