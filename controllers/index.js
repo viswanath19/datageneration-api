8b030c7d1b67e48d6e8a7cfe2373e4ef847470a0
Viswanath <allamrajuviswanath@gmail.com>
2023-09-16 21:39:39 +0530

    update row fetch logic

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,7 @@ mainRouter.get("/",(req,res) => {
 })
 
 mainRouter.get("/generateNames",async (req,res)=>{
+    console.log("came here");
     let names = "<table><tr><th>Name</th><th>Email Address</th><th>Phone Number</th></tr>";
     const offset = Math.floor(Math.random()*(35000-1)+1);
     const fetchNames = await dbClient.query("SELECT people_name FROM dataccrue_dataset.name_dataset limit 50 OFFSET "+offset).then(results => {
@@ -15,9 +16,10 @@ mainRouter.get("/generateNames",async (req,res)=>{
     }).catch(err => {
         console.log("Error Occured While Fetching in Names db",err);
     });
-    const fetchEmails = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.email_dataset").then(results => {
+    const fetchEmailsCounts = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.email_dataset").then(results => {
+        console.log("no of email rows",results.rows);
         const offset = Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
-        dbClient.query("SELECT email_addresses FROM dataccrue_dataset.email_dataset limit 50 OFFSET "+offset).then(results => {
+        dbClient.query("SELECT email_addresses FROM dataccrue_dataset.email_dataset limit 50 OFFSET "+offset-50).then(results => {
             console.log("results as follows from emails db",results.rowCount);
             return results.rows;
         }).catch(err => {
@@ -29,7 +31,7 @@ mainRouter.get("/generateNames",async (req,res)=>{
     
     const fetchPhoneNumbers = await dbClient.query("SELECT count(*) FROM dataccrue_dataset.phone_dataset").then(results => {
         const offset = Math.floor(Math.random()*(results.rows[0]['count']-1)+1);
-        dbClient.query("SELECT mobile_numbers FROM dataccrue_dataset.phone_dataset limit 50 OFFSET "+offset).then(results => {
+        dbClient.query("SELECT mobile_numbers FROM dataccrue_dataset.phone_dataset limit 50 OFFSET "+offset-50).then(results => {
             console.log("results as follows from phones db",results.rowCount);
             return results.rows;
         }).catch(err => {
@@ -38,8 +40,10 @@ mainRouter.get("/generateNames",async (req,res)=>{
     }).catch(err => {
         console.log("Error Occured While Fetching in phone db counts",err);
     });
+    console.log("fetch emails",fetchEmails);
     if (fetchNames !== null && fetchNames.length > 0) {
         fetchNames.forEach((rowItem,index) => {
+            console.log(fetchEmails[index])
             names = names + `<tr>
                     <td>${rowItem['people_name']}</td>
                     <td>${fetchEmails[index]["email_addresses"]}</td>
